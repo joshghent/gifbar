be901ef299f12c92bd6d7392e4be76a74e4c161b
Felix Chin <felixmchin@gmail.com>
2020-10-21 17:40:03 -0700

    refactored Spinner to functional component

--- components/spinner.jsx
+++ components/spinner.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
 
-export default class Spinner extends React.Component {
-	render() {
-		return (
-			<div className='spinner'>
-				<ReactLoading type='bars' />
-			</div>
-		);
-	}
+const Spinner = () => {
+  return (
+    <div className='spinner'>
+      <ReactLoading type='bars' />
+    </div>
+  )
 }
+
+export default Spinner;
