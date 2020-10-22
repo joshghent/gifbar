87db27c85684d4e272864df3278285fe571fade8
Felix Chin <felixmchin@gmail.com>
2020-10-21 19:22:02 -0700

    refactored gifList as functional component

--- components/gifList.jsx
+++ components/gifList.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
-import GifItem from './gifItem.jsx';
-import  map from 'lodash.map';
+import map from 'lodash.map';
+import GifItem from './gifItem';
 
-export default class GifList extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+const GifList = ({ gifs, handleGifClick, copied }) => {
+  const gifNodes = map(gifs, (gif, index) => (
+    <GifItem
+      gif={gif}
+      key={index}
+      gifId={index}
+      onGifClick={(gifId) => handleGifClick(gifId)}
+      isCopied={copied === index}
+    />
+  ));
+  return (
+    <div>
+      <img src="./../assets/giphy-mark.png" className="giphy-watermark" alt="" />
+      <div className="gif-list">{gifNodes}</div>
+    </div>
+  );
+};
 
-	render() {
-		const gifNodes = map(this.props.gifs, (gif, index) => {
-			return (
-				<GifItem
-					gif={gif}
-					key={index}
-					gifId={index}
-					onGifClick={(gifId) => this.props.handleGifClick(gifId)}
-					isCopied={this.props.copied === index}
-				/>
-			);
-		});
-
-		return (
-			<div>
-				<img src='./../assets/giphy-mark.png' className='giphy-watermark' />
-				<div className='gif-list'>{gifNodes}</div>
-			</div>
-		);
-	}
-}
+export default GifList;
